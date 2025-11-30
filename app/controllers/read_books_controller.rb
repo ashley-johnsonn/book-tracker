a7c48ac2887ddecf0fd84e2c8ee0f8929932cc17
Ashley Johnson <ashley.duysen@gmail.com>
2025-12-01 09:01:17 +1000

    Created add new book view and form.

--- app/controllers/read_books_controller.rb
+++ app/controllers/read_books_controller.rb
@@ -6,7 +6,9 @@ class ReadBooksController < ApplicationController
   def show
   end
 
+  # Rails will automatically render app/views/read_books/new.html.erb :)
   def new
+    @read_book = ReadBook.new
   end
 
   def create
