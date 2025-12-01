beedf950b6bca45257ecf715bf076820a7a89629
Ashley Johnson <ashley.duysen@gmail.com>
2025-12-01 10:00:09 +1000

    created show view

--- app/controllers/read_books_controller.rb
+++ app/controllers/read_books_controller.rb
@@ -4,6 +4,7 @@ class ReadBooksController < ApplicationController
   end
 
   def show
+    @read_book = ReadBook.find(params[:id])
   end
 
   # Rails will automatically render app/views/read_books/new.html.erb :)
@@ -12,5 +13,18 @@ class ReadBooksController < ApplicationController
   end
 
   def create
+    @read_book = ReadBook.new(read_book_params)
+    if @read_book.save
+      # redirect somewhere on success
+      redirect_to read_books_path
+    else 
+      # render the form again on failure
+    end
   end
+end
+
+private
+
+def read_book_params
+  params.require(:read_book).permit(:title, :author, :date_published, :description, :rating, :date_finished)
 end
