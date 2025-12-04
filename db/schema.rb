48b7ab4f9aa6e356e5b7a09ec218794c2b641972
Ashley Johnson <ashley.duysen@gmail.com>
2025-12-05 08:32:57 +1000

    added user model and bcript gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_02_002147) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_04_220357) do
   create_table "read_books", force: :cascade do |t|
     t.string "author"
     t.string "cover_image_url"
@@ -21,5 +21,14 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_02_002147) do
     t.string "title"
     t.datetime "updated_at", null: false
     t.integer "year_published"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "updated_at", null: false
+    t.string "username"
   end
 end
