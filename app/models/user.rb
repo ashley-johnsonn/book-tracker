b69aef592252e135dafd5f37f0df8ab821947a6b
Ashley Johnson <ashley.duysen@gmail.com>
2025-12-05 10:45:03 +1000

    Login/signup validationnn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :read_books
+
+  # Validations
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 8 }, on: :create
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true
 end
